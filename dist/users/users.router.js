5835d20aa12cee8d70f9a85bae0d17fc83efebd4
Netto Cavalcante <frcnetto@gmail.com>
2018-11-17 19:53:11 -0300

    Refactoring in routes
    Making abstract routes for reuse in the future.

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const router_1 = require("../common/router");
 const users_model_1 = require("./users.model");
-const restify_errors_1 = require("restify-errors");
-class UsersRouter extends router_1.Router {
+const model_router_1 = require("../common/model-router");
+class UsersRouter extends model_router_1.ModelRouter {
     constructor() {
-        super();
+        super(users_model_1.User);
         this.usersNode = '/users';
         this.usersIdNode = this.usersNode + '/:id';
         this.on('beforeRender', document => {
@@ -13,53 +12,12 @@ class UsersRouter extends router_1.Router {
         });
     }
     applyRoutes(application) {
-        application.get(this.usersNode, (req, res, next) => {
-            users_model_1.User.find()
-                .then(this.render(res, next))
-                .catch(next);
-        });
-        application.get(this.usersIdNode, (req, res, next) => {
-            users_model_1.User.findById(req.params.id)
-                .then(this.render(res, next))
-                .catch(next);
-        });
-        application.post(this.usersNode, (req, res, next) => {
-            let user = new users_model_1.User(req.body);
-            user.save()
-                .then(this.render(res, next))
-                .catch(next);
-        });
-        application.put(this.usersIdNode, (req, res, next) => {
-            const options = { runValidators: true, overwrite: true };
-            users_model_1.User.update({ _id: req.params.id }, req.body, options)
-                .exec()
-                .then(result => {
-                if (result.n) {
-                    return users_model_1.User.findById(req.params.id);
-                }
-                else {
-                    throw new restify_errors_1.NotFoundError('Documento não encontrado.');
-                }
-            })
-                .then(this.render(res, next))
-                .catch(next);
-        });
-        application.patch(this.usersIdNode, (req, res, next) => {
-            const options = { runValidators: true, new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(res, next))
-                .catch(next);
-        });
-        application.del(this.usersIdNode, (req, res, next) => {
-            users_model_1.User.deleteOne({ _id: req.params.id }).exec().then(result => {
-                console.log(result);
-                if (result.n)
-                    res.send(204);
-                else
-                    throw new restify_errors_1.NotFoundError('Documento não encontrado.');
-                return next();
-            });
-        });
+        application.get(this.usersNode, this.findAll);
+        application.get(this.usersIdNode, [this.validateId, this.findById]);
+        application.post(this.usersNode, this.save);
+        application.put(this.usersIdNode, [this.validateId, this.replace]);
+        application.patch(this.usersIdNode, [this.validateId, this.update]);
+        application.del(this.usersIdNode, [this.validateId, this.delete]);
     }
 }
 exports.usersRouter = new UsersRouter();
